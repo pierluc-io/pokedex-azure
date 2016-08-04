d7c83dcb17618fa0abbce805340405b89a0c42b9
Pier-Luc Gendreau <Zertz@users.noreply.github.com>
2016-08-04 16:36:15 -0400

    feat(fn): add pokeapi-resource-list (#1)
    * Add pokeapi-resource-list
    
    * Implement with sync/whilst

---
+++ pokeapi-resource-list/index.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const request = require('request')
+const whilst = require('async/whilst')
+
+module.exports = function (context, req) {
+  const resource = req.query && req.query.resource ? req.query.resource : null
+  const json = true
+
+  let uri = `https://pokeapi.co/api/v2/${resource}/`
+  let results
+
+  if (!resource) {
+    context.res = {
+      status: 400,
+      message: '"resource" must be specified'
+    }
+  }
+
+  whilst(() => !!uri, (cb) => {
+    request({ uri, json }, (err, response, body) => {
+      if (err) {
+        return cb(err)
+      }
+
+      uri = body.next
+
+      cb(null, results.concat(body.results))
+    })
+  }, (err, results) => {
+    if (err) {
+      context.res = {
+        status: 500,
+        message: err.message
+      }
+    } else {
+      const count = results.length
+
+      context.res = { count, results }
+    }
+
+    context.done()
+  })
+}
